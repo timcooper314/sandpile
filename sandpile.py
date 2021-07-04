d12002ba1d7cdd56a28fe6b3edff6eacd4b122fe
Tim Cooper <timcooper314@gmail.com>
2021-07-04 16:09:52 +1000

    Misc refactoring and reformatting.

--- sandpile.py
+++ sandpile.py
@@ -7,7 +7,7 @@ from scipy.optimize import curve_fit
 from math import ceil
 
 
-class Table():
+class Table:
     def __init__(self, M, N, k):
         self.M = M  # number of rows
         self.N = N  # number of columns
@@ -27,106 +27,99 @@ class Table():
         else:
             self.grid[args[0], args[1]] += 1
 
-    def check_site(self, m, n):
-        # Checks if any avalanches will occur at (m,n)
-        if self.grid[m, n] == self.k:  # avalanche
-            # print("avalanche")
-            self.m = m
-            self.n = n
+    def check_site(self, i_check, j_check):
+        """Checks if any avalanches will occur at (m,n)"""
+        if self.grid[i_check, j_check] == self.k:  # avalanche
             return True
         else:  # No avalanche
             return False
 
-    def execute_avalanche(self, m, n):
-        # Function to execute the avalanche starting at the point (m,n)
-        self.s = 0  # Avalanche size - number of grains displaced during avalanche
-        self.t = 0  # Avalanche lifetime- nuber of timetsteps taken to relax to critical state
-        self.a = 1  # Avalanche area- number of unique sitest toppled
-        self.r = 0  # Avalanche radius- max number of sites away from the initial point that the avalanche reaches
-        originPt = [m, n]  # Avalanche starts at m,n
-        siteDistances = []  # A list of the distances of toppling sites from origin
-        topplePts = [[m, n]]  # A list for storing surrounding pts which need to be toppled
-        toppleSites = [[m,n]]  # Sites to be toppled
-        while topplePts != []:
-            for Pt in topplePts:
-                i, j = Pt[0], Pt[1]
+    def execute_avalanche(self, i_0, j_0):
+        """Execute the avalanche starting at the point (m,n)"""
+        a_size = 0  # Avalanche size - number of grains displaced during avalanche
+        a_time = 0  # Avalanche lifetime- number of time-steps taken to relax to critical state
+        origin_pt = [i_0, j_0]  # Avalanche starts at (i_0, j_0)
+        site_distances = []  # A list of the distances of toppling sites from origin
+        topple_pts = [[i_0, j_0]]  # A list for storing surrounding pts which need to be toppled
+        topple_sites = [[i_0, j_0]]  # Sites to be toppled
+        while topple_pts:
+            for pt in topple_pts:
+                i, j = pt
                 self.topple(i, j)
-                if Pt not in toppleSites:  # A list of unique pts toppled in the avalance
-                    toppleSites.append([i, j])
-                distance = abs(originPt[0]-i) + abs(originPt[1] -j) # x+y distance from origin to topple pt.
-                siteDistances.append(distance)
-                topplePts = topplePts[1:]  # remove point just toppled
-                self.s += 4  # 2d=4 grains displaced per topple
-                surroundingPts = [[i+1, j], [i-1, j], [i, j+1], [i, j-1]]
-                for site in surroundingPts:  # Now check all cells surrounding, to see if avalanches will occur
-                    r,c = site[0], site[1]
+                if pt not in topple_sites:  # A list of unique pts toppled in the avalanche
+                    topple_sites.append([i, j])
+                distance = abs(origin_pt[0]-i) + abs(origin_pt[1] - j)  # x+y distance from origin to topple pt.
+                site_distances.append(distance)
+                topple_pts = topple_pts[1:]  # remove point just toppled
+                a_size += 4  # 2d=4 grains displaced per topple
+                surrounding_pts = [[i+1, j], [i-1, j], [i, j+1], [i, j-1]]
+                for site in surrounding_pts:  # Now check all cells surrounding, to see if avalanches will occur
+                    r, c = site
                     if r == 0 or r == self.M + 1 or c == 0 or c == self.N + 1:  # If sand fell off table
                         pass
                     elif self.check_site(r, c):
-                        topplePts.append([r, c])
-                # print(self.grid)
-            self.t += 1     # Count a timestep
+                        topple_pts.append([r, c])
+            a_time += 1  # Count a time-step
         # End of avalanche. Return statistics
-        self.a = len(toppleSites)
-        self.r = max(siteDistances)
-        return([self.s, self.t, self.a, self.r])
+        a_area = len(topple_sites)  # Avalanche area- number of unique sites toppled
+        a_radius = max(site_distances)  # Avalanche radius- max number of sites away from the initial point that the avalanche reaches
+        return {'size': a_size, 'lifetime': a_time, 'area': a_area, 'radius': a_radius}
 
-    def topple(self, m, n):
-        # Function to perform a toppling process at the grid point (m,n)
-        self.surrounds = [[m+1, n], [m-1, n], [m, n+1], [m, n-1]]
-        self.grid[m, n] -= 4  # 4 grains topple
-        for pt in self.surrounds:
+    def topple(self, i_topple, j_topple):
+        """Perform a toppling process at the grid point (m,n)"""
+        surrounds = [[i_topple+1, j_topple], [i_topple-1, j_topple], [i_topple, j_topple+1], [i_topple, j_topple-1]]
+        self.grid[i_topple, j_topple] -= 4  # 4 grains topple
+        for pt in surrounds:
             self.grid[pt[0], pt[1]] += 1  # surroundings gain a grain
 
 
 def get_freq_data(data):
-    # Sorts data ( an array of statistics) into unique data points and the number of each data point
-    d = np.array(data)
-    freq = np.bincount(d)
-    uniqueVal = np.nonzero(freq)[0]
-    d1 = np.vstack((uniqueVal, freq[uniqueVal])).T
-    return([d1[:, 0], d1[:, 1]])
+    """Sorts data ( an array of statistics) into unique data points and the number of each data point"""
+    freq = np.bincount(np.array(data))
+    unique_val = np.nonzero(freq)[0]
+    d1 = np.vstack((unique_val, freq[unique_val])).T
+    return [d1[:, 0], d1[:, 1]]
 
 
-def plot_histogram(data, observable, xunits):
-    # plots a histogram of the data (array of stats) of an ["observable","units of observable"]
+def plot_histogram(data, observable, x_units):
+    """Plots a histogram of data (array of stats) of an ["observable", "units of observable"]"""
     plt.hist(data, max(data))
-    plt.title("Avalanche %s for grid size = %d x %d" %(observable, gridSize[0], gridSize[1]))
-    plt.xlabel("%s (%s)" %(observable, xunits))
+    plt.title(f"Avalanche {observable} for grid size = {grid_config[0]} x {grid_config[1]}")
+    plt.xlabel(f"{observable} ({x_units})")
     plt.ylabel("Number of avalanches")
     plt.show()
 
 
-def loglog_plot_stats(data, observable, xunits):
-    # Produces loglog plot of the distribution of an observables data
-    # Data is an array of stats, observable and xunits are strings
+def loglog_plot_stats(data, observable, x_units):
+    """Produces loglog plot of the distribution of an observables data
+    Data is an array of stats, observable and x_units are strings"""
     [x, freq] = get_freq_data(data)
     plt.loglog(x, freq, '.', basex=10)
-    plt.title("Avalanche distribution for %s (Grid size: %dx%d )" %(observable, gridSize[0], gridSize[1]))
+    plt.title(f"Avalanche distribution for {observable} (Grid size: {grid_config[0]}x{grid_config[1]} )")
     plt.ylabel("Number of avalanches")
-    plt.xlabel("%s (%s)"%(observable, xunits))
+    plt.xlabel(f"{observable} ({x_units})")
     plt.show()
 
 
-def power_law_fit_plot(xdata, ydata, xylegend, unitsLegend):
-    # Power law fits between xdata and ydata
-    # xylegend = ["name of xdata","name of ydata"],#units legend=[units of xdata,units of ydata]
-    xData = np.array(xdata)
-    params, pcov = curve_fit(power_law_func, xData, ydata)
-    plt.plot(xData, ydata, ".")
+def power_law_fit_plot(x_data, y_data, xy_legend, units_legend):
+    """Power law fits between x_data and y_data
+    xy_legend = ["name of x_data","name of y_data"], #units legend=[units of x_data,units of y_data]"""
+    xData = np.array(x_data)
+    params, p_cov = curve_fit(power_law_func, xData, y_data)
+    plt.plot(xData, y_data, ".")
     plt.plot(np.sort(xData), power_law_func(np.sort(xData), *params), '-')
-    plt.legend(["Avalanche data", "Power- law fit: x^%5.3f" %(params[1])])
-    plt.title("Relationship between %s and %s" %(xylegend[0], xylegend[1]))
-    plt.xlabel("%s (%s)" %(xylegend[0], unitsLegend[0]))
-    plt.ylabel("%s (%s)" %(xylegend[1], unitsLegend[1]))
+    plt.legend(["Avalanche data", "Power-law fit: x^%5.3f" % (params[1])])
+    plt.title(f"Relationship between {xy_legend[0]} and {xy_legend[1]}")
+    plt.xlabel(f"{xy_legend[0]} ({units_legend[0]})")
+    plt.ylabel(f"{xy_legend[1]} ({units_legend[1]})")
     plt.show()
 
 
-def power_law_func(x, a, b):  # power law function to fit data
+def power_law_func(x, a, b):
     return a * x**b
 
 
-def exp_func(x, a, b, c):
+def exp_func(x, a, b):
     return a*np.exp(-b*x)
 
 
@@ -134,13 +127,12 @@ if __name__ == '__main__':
     legend = []
     allDensities = []  # array for collecting grid densities of sand
     plotOn = 1  # 0 for no plots, 1 for plots to be shown
-    for gridSize in [[11, 11]]:
-        legend.append("%d x %d grid" % (gridSize[0], gridSize[1]))
-        sandpile = Table(gridSize[0], gridSize[1], 4)
+    for grid_config in [[11, 11]]:
+        legend.append(f"{grid_config[0]} x {grid_config[1]} grid")
+        sandpile = Table(grid_config[0], grid_config[1], 4)
         # print(sandpile.grid)
-        totalGrains = 1000  # Number of grains to be dropped
-        time = 0  # count timesteps
-        size = []     # Statistics to be collected
+        totalGrains = 1000  # Number of  grains to be dropped
+        size = []  # Statistics to be collected
         lifetime = []
         area = []
         radius = []
@@ -148,64 +140,57 @@ if __name__ == '__main__':
         # Loop over number of grains to be dropped:
         for grain in range(totalGrains):
             sandpile.add_grain()
-            time += 1  # add timestep for each grain dropped
             sandGrid = sandpile.grid[1:sandpile.M+1, 1:sandpile.N+1]  # grid without edges
             m, n = np.unravel_index(sandGrid.argmax(), sandGrid.shape)  # Find site with max grains:
-            m = m + 1
-            n = n + 1
+            m += 1
+            n += 1
             # Check if avalanche will occur:
             if sandpile.check_site(m, n):
                 stats = sandpile.execute_avalanche(m, n)  # Avalanche occurs at (m,n)
                 # store statistics for avalanche:
-                size.append(stats[0])
-                lifetime.append(stats[1])
-                area.append(stats[2])
-                radius.append(stats[3])
+                size.append(stats['size'])
+                lifetime.append(stats['lifetime'])
+                area.append(stats['area'])
+                radius.append(stats['radius'])
                 density.append(np.average(sandpile.grid[1:sandpile.M+1, 1:sandpile.N+1]))
-                totalSteps = time + sandpile.t
+                totalSteps = grain + 1 + stats['lifetime']
         allDensities.append(density)
 
-
         #####################
-        #Plots:
+        # Plots:
         if plotOn:
             obsLegend = ["Size", "Lifetime", "Area", "Radius"]
-            unitsLegend = ["number of sites", "time units", "number of sites", "number of sites"]  # units for each observable
+            units_legend = ["number of sites", "time units", "number of sites", "number of sites"]
             # Histogram plots and loglog plots for observables vs number avalanches:
-            i = 0
-            for stat in [size, lifetime, area, radius]:
-                plot_histogram(stat, obsLegend[i], unitsLegend[i])
-                loglog_plot_stats(stat, obsLegend[i], unitsLegend[i])
-                i += 1
+            for i, stat in enumerate([size, lifetime, area, radius]):
+                plot_histogram(stat, obsLegend[i], units_legend[i])
+                loglog_plot_stats(stat, obsLegend[i], units_legend[i])
 
             # Power law fit of observables vs number of avalanches
-            i = 0
-            for stat in [size, lifetime, area]:
+            for i, stat in enumerate([size, lifetime, area]):
                 [stats, freqs] = get_freq_data(stat)
-                power_law_fit_plot(stats, freqs, [obsLegend[i], "Number of avalanches"], [unitsLegend[i],"Number"])
-                i += 1
+                power_law_fit_plot(stats, freqs, [obsLegend[i], "Number of avalanches"], [units_legend[i], "Number"])
 
             # Consider radius separately:  remove zero term from radius in order to get power law fit
             [radii, freqs] = get_freq_data(radius)
-            radii, freqs  = radii[1:], freqs[1:]
-            power_law_fit_plot(radii,freqs, [obsLegend[3], "Number of avalanches"], [unitsLegend[i], "Number"])
+            radii, freqs = radii[1:], freqs[1:]
+            power_law_fit_plot(radii, freqs, [obsLegend[3], "Number of avalanches"], [units_legend[i], "Number"])
             # try exponential fit for radius instead:
-            params, pcov = curve_fit(exp_func, radii, freqs)
+            params, p_cov = curve_fit(exp_func, radii, freqs)
             plt.plot(radii, freqs, ".")
             plt.plot(np.sort(radii), exp_func(np.sort(radii), *params), '-')
-            plt.legend(["Avalanche data", "Exponential fit: ~ exp(-%5.3fx)" %(params[1])])
-            plt.title("Scaling law between %s and %s" %(obsLegend[3], "Number of avalanches"))
-            plt.xlabel("%s (%s)" %(obsLegend[3], unitsLegend[3]))
+            plt.legend(["Avalanche data", "Exponential fit: ~ exp(-%5.3fx)" % (params[1])])
+            plt.title(f"Scaling law between {obsLegend[3]} and Number of avalanches")
+            plt.xlabel(f"{obsLegend[3]} ({units_legend[3]})")
             plt.ylabel("Number of avalanches")
             plt.show()
 
             # Power law fits of size vs other observables
             [sizes, freqs] = get_freq_data(size)
-            i = 1
-            for stat in [lifetime, area, radius]:
-                [stats, statFreqs] = get_freq_data(stat)
-                power_law_fit_plot(size, stat, [obsLegend[0], obsLegend[i]], [unitsLegend[0], unitsLegend[i]])  # correlations between  each variable and avalanche size :
-                i += 1
+            for i, stat in enumerate([lifetime, area, radius], 1):
+                [stats, stat_freqs] = get_freq_data(stat)
+                # correlations between  each variable and avalanche size :
+                power_law_fit_plot(size, stat, [obsLegend[0], obsLegend[i]], [units_legend[0], units_legend[i]])
             ##############
 
     # Sand densities plots
@@ -221,7 +206,7 @@ if __name__ == '__main__':
         # Grid surface plot of final configuration
     sandGrid = sandpile.grid[1:sandpile.M+1, 1:sandpile.N+1]  # grid without edges
     plt.imshow(sandGrid, interpolation='nearest', cmap='Blues')
-    plt.title("Surface Density of Sandpile (Number of Grains: %d)" %(totalGrains))
+    plt.title(f"Surface Density of Sandpile (Number of Grains: {totalGrains})")
     plt.colorbar()
     plt.show()
     #############################
--- sandpile_gui.py
+++ sandpile_gui.py
@@ -23,22 +23,22 @@ class Window(QDialog):  # ):#QMainWindow):
         self.N = 25
         self.k = 4
         self.sandPile = sandpile.Table(self.M, self.N, self.k)
-        self.initUI()
+        self.init_ui()
 
-    def initUI(self):
+    def init_ui(self):
         self.setWindowTitle("Abelian Sandpile")
         self.setGeometry(100, 100, 300, 300)  # x,y pos, width,height
-        self.windowLayout = QVBoxLayout()
-        self.createGridLayout()
-        self.createHorizontalLayout()
+        self.window_layout = QVBoxLayout()
+        self.create_grid_layout()
+        self.create_horizontal_layout()
 
-        self.windowLayout.addWidget(self.gridGroupBox)
-        self.windowLayout.addWidget(self.horizontalGroupBox)
-        self.setLayout(self.windowLayout)
+        self.window_layout.addWidget(self.grid_group_box)
+        self.window_layout.addWidget(self.horizontal_group_box)
+        self.setLayout(self.window_layout)
         self.show()
 
-    def createGridLayout(self):
-        self.gridGroupBox = QGroupBox("Grid")
+    def create_grid_layout(self):
+        self.grid_group_box = QGroupBox("Grid")
         layout = QGridLayout()
         layout.setSpacing(0)
         self.buttons = []
@@ -50,46 +50,46 @@ class Window(QDialog):  # ):#QMainWindow):
         else:
             self.colours = {0: "white"}
             for c in range(1, self.k):
-                self.colours[c] = "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+                self.colours[c] = "#"+''.join([random.choice('0123456789ABCDEF') for _ in range(6)])
         ind = 0
         for i in range(self.M):
             for j in range(self.N):
                 self.buttons.append(QPushButton())  # f'{0}'))
-                self.buttons[ind].clicked.connect(partial(self.addGrain, i, j))
+                self.buttons[ind].clicked.connect(partial(self.add_grain, i, j))
                 self.buttons[ind].setStyleSheet("background-color : white")
                 layout.addWidget(self.buttons[ind], i, j)  # , 2, 1)
                 ind += 1
-        self.gridGroupBox.setLayout(layout)
+        self.grid_group_box.setLayout(layout)
 
-    def createHorizontalLayout(self):
-        self.horizontalGroupBox = QGroupBox("horizontal")
+    def create_horizontal_layout(self):
+        self.horizontal_group_box = QGroupBox("horizontal")
         layout = QHBoxLayout()
 
-        self.numberGrainsText = QLineEdit("Number of grains", self)
-        layout.addWidget(self.numberGrainsText)
+        self.number_grains_text = QLineEdit("Number of grains", self)
+        layout.addWidget(self.number_grains_text)
 
-        self.randomRadioButton = QRadioButton("Random", self)
-        layout.addWidget(self.randomRadioButton)
+        self.random_radio_button = QRadioButton("Random", self)
+        layout.addWidget(self.random_radio_button)
 
-        startButton = QPushButton("Start", self)
-        startButton.clicked.connect(partial(self.startClick))  # ,totalGrains=self.numberGrainsText.text(),randomPos = self.randomRadioButton.isChecked()))
-        layout.addWidget(startButton)
+        start_button = QPushButton("Start", self)
+        start_button.clicked.connect(partial(self.start_click))  # ,total_grains=self.number_grains_text.text(),random_pos = self.random_radio_button.isChecked()))
+        layout.addWidget(start_button)
 
-        resetButton = QPushButton("Clear", self)
-        resetButton.clicked.connect(self.clearGrid)
-        layout.addWidget(resetButton)
+        reset_button = QPushButton("Clear", self)
+        reset_button.clicked.connect(self.clear_grid)
+        layout.addWidget(reset_button)
 
-        self.horizontalGroupBox.setLayout(layout)
+        self.horizontal_group_box.setLayout(layout)
 
-    def addGrain(self, i, j):
+    def add_grain(self, i, j):
         self.sandPile.grid[i+1][j+1] += 1
-        sandGrid = self.sandPile.grid[1:self.M+1, 1:self.N+1]  # grid without edges
-        m, n = np.unravel_index(sandGrid.argmax(), sandGrid.shape)  # Find site with max grains:
+        sandgrid = self.sandPile.grid[1:self.M+1, 1:self.N+1]  # grid without edges
+        m, n = np.unravel_index(sandgrid.argmax(), sandgrid.shape)  # Find site with max grains:
         m = m+1
         n = n+1
 
         if self.sandPile.check_site(m, n):  # Avalanche
-            stats = self.sandPile.execute_avalanche(m, n)  # Avalanche occurs at (m,n)
+            _ = self.sandPile.execute_avalanche(m, n)  # Avalanche occurs at (m,n)
             for i in range(self.M):
                 for j in range(self.N):
                     # self.buttons[i*self.N+j].setText(f'{self.sandPile.grid[i+1][j+1]}')
@@ -97,34 +97,34 @@ class Window(QDialog):  # ):#QMainWindow):
         else:  # no avalanche
             self.buttons[i*self.N+j].setStyleSheet("background-color : "+self.colours[self.sandPile.grid[i+1][j+1]])
 
-    def startClick(self):
-        totalGrains = self.numberGrainsText.text()
-        randomPos = self.randomRadioButton.isChecked()
-        if totalGrains == "Number of grains":
-            totalGrains = 100  # default
-            print(f"Default number of grains: {totalGrains}")
+    def start_click(self):
+        total_grains = self.number_grains_text.text()
+        random_pos = self.random_radio_button.isChecked()
+        if total_grains == "Number of grains":
+            total_grains = 100  # default
+            print(f"Default number of grains: {total_grains}")
         else:
-            totalGrains = int(totalGrains)
+            total_grains = int(total_grains)
 
-        if randomPos:
-            pos_i = np.random.randint(self.M, size=totalGrains)
-            pos_j = np.random.randint(self.N, size=totalGrains)
+        if random_pos:
+            pos_i = np.random.randint(self.M, size=total_grains)
+            pos_j = np.random.randint(self.N, size=total_grains)
         elif np.sum(self.sandPile.grid) == 0:
             print("Simulating grains dropped in center")
-            pos_i = self.M//2 * np.ones(totalGrains, dtype=int)
-            pos_j = self.N//2 * np.ones(totalGrains, dtype=int)
+            pos_i = self.M//2 * np.ones(total_grains, dtype=int)
+            pos_j = self.N//2 * np.ones(total_grains, dtype=int)
         elif np.sum(self.sandPile.grid) == 1:  # start on initial point
             print("Starting on initial seed")
             [i_index, j_index] = np.where(self.sandPile.grid == 1)
-            pos_i = (i_index-1) * np.ones(totalGrains, dtype=int)
-            pos_j = (j_index-1) * np.ones(totalGrains, dtype=int)
+            pos_i = (i_index-1) * np.ones(total_grains, dtype=int)
+            pos_j = (j_index-1) * np.ones(total_grains, dtype=int)
         else:
             print("Choose only one starting cell")
             return
-        for grain in range(totalGrains):
-            self.addGrain(pos_i[grain], pos_j[grain])
+        for grain in range(total_grains):
+            self.add_grain(pos_i[grain], pos_j[grain])
 
-    def clearGrid(self):
+    def clear_grid(self):
         plt.imshow(self.sandPile.grid[1:self.M+1, 1:self.N+1], cmap='Blues', interpolation='nearest')  # ,shading='gouraud')
         plt.show()
         self.sandPile.grid = np.zeros([self.M+2, self.N+2], dtype=int)
