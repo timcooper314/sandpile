e38af278b80d2e3dc105aa8632f6ee0b9a73ad52
Tim Cooper <timcooper314@gmail.com>
2021-07-04 11:47:09 +1000

    Minor PEP formatting edits.

--- sandpile.py
+++ sandpile.py
@@ -15,7 +15,7 @@ class Table():
         # Initialise M by N grid of zeroes:
         self.grid = np.zeros([M+2, N+2], dtype=int)  # extra rows and columns around edges for overflow
 
-    def add_grain(self,*args):
+    def add_grain(self, *args):
         # Function to add a single grain of sand to the table in a random site (m,n)
         # optional args for position of grain
         # m = random.randint(1,self.M)
@@ -57,10 +57,10 @@ class Table():
                 siteDistances.append(distance)
                 topplePts = topplePts[1:]  # remove point just toppled
                 self.s += 4  # 2d=4 grains displaced per topple
-                surroundingPts = [[i+1,j],[i-1,j],[i,j+1],[i,j-1]]
+                surroundingPts = [[i+1, j], [i-1, j], [i, j+1], [i, j-1]]
                 for site in surroundingPts:  # Now check all cells surrounding, to see if avalanches will occur
                     r,c = site[0], site[1]
-                    if r == 0 or r == self.M + 1 or c == 0 or c==self.N + 1:  # If sand fell off table
+                    if r == 0 or r == self.M + 1 or c == 0 or c == self.N + 1:  # If sand fell off table
                         pass
                     elif self.check_site(r, c):
                         topplePts.append([r, c])
@@ -84,15 +84,15 @@ def get_freq_data(data):
     d = np.array(data)
     freq = np.bincount(d)
     uniqueVal = np.nonzero(freq)[0]
-    d1 = np.vstack((uniqueVal,freq[uniqueVal])).T
-    return([d1[:,0],d1[:,1]])
+    d1 = np.vstack((uniqueVal, freq[uniqueVal])).T
+    return([d1[:, 0], d1[:, 1]])
 
 
-def plot_histogram(data, observable,xunits):
+def plot_histogram(data, observable, xunits):
     # plots a histogram of the data (array of stats) of an ["observable","units of observable"]
-    plt.hist(data,max(data))
-    plt.title("Avalanche %s for grid size = %d x %d" %(observable,gridSize[0],gridSize[1]))
-    plt.xlabel("%s (%s)" %(observable,xunits))
+    plt.hist(data, max(data))
+    plt.title("Avalanche %s for grid size = %d x %d" %(observable, gridSize[0], gridSize[1]))
+    plt.xlabel("%s (%s)" %(observable, xunits))
     plt.ylabel("Number of avalanches")
     plt.show()
 
@@ -100,11 +100,11 @@ def plot_histogram(data, observable,xunits):
 def loglog_plot_stats(data, observable, xunits):
     # Produces loglog plot of the distribution of an observables data
     # Data is an array of stats, observable and xunits are strings
-    [x,freq] = get_freq_data(data)
-    plt.loglog(x,freq, '.',basex = 10)
-    plt.title("Avalanche distribution for %s (Grid size: %dx%d )" %(observable,gridSize[0],gridSize[1]))
+    [x, freq] = get_freq_data(data)
+    plt.loglog(x, freq, '.', basex=10)
+    plt.title("Avalanche distribution for %s (Grid size: %dx%d )" %(observable, gridSize[0], gridSize[1]))
     plt.ylabel("Number of avalanches")
-    plt.xlabel("%s (%s)"%(observable,xunits))
+    plt.xlabel("%s (%s)"%(observable, xunits))
     plt.show()
 
 
@@ -113,10 +113,10 @@ def power_law_fit_plot(xdata, ydata, xylegend, unitsLegend):
     # xylegend = ["name of xdata","name of ydata"],#units legend=[units of xdata,units of ydata]
     xData = np.array(xdata)
     params, pcov = curve_fit(power_law_func, xData, ydata)
-    plt.plot(xData,ydata,".")
+    plt.plot(xData, ydata, ".")
     plt.plot(np.sort(xData), power_law_func(np.sort(xData), *params), '-')
-    plt.legend(["Avalanche data","Power- law fit: x^%5.3f" %(params[1])])
-    plt.title("Relationship between %s and %s" %(xylegend[0],xylegend[1]))
+    plt.legend(["Avalanche data", "Power- law fit: x^%5.3f" %(params[1])])
+    plt.title("Relationship between %s and %s" %(xylegend[0], xylegend[1]))
     plt.xlabel("%s (%s)" %(xylegend[0], unitsLegend[0]))
     plt.ylabel("%s (%s)" %(xylegend[1], unitsLegend[1]))
     plt.show()
@@ -126,7 +126,7 @@ def power_law_func(x, a, b):  # power law function to fit data
     return a * x**b
 
 
-def exp_func(x,a,b,c):
+def exp_func(x, a, b, c):
     return a*np.exp(-b*x)
 
 
@@ -150,7 +150,7 @@ if __name__ == '__main__':
             sandpile.add_grain()
             time += 1  # add timestep for each grain dropped
             sandGrid = sandpile.grid[1:sandpile.M+1, 1:sandpile.N+1]  # grid without edges
-            m, n = np.unravel_index((sandGrid).argmax(), (sandGrid).shape)  # Find site with max grains:
+            m, n = np.unravel_index(sandGrid.argmax(), sandGrid.shape)  # Find site with max grains:
             m = m + 1
             n = n + 1
             # Check if avalanche will occur:
@@ -161,7 +161,7 @@ if __name__ == '__main__':
                 lifetime.append(stats[1])
                 area.append(stats[2])
                 radius.append(stats[3])
-                density.append(np.average(sandpile.grid[1:sandpile.M+1,1:sandpile.N+1]))
+                density.append(np.average(sandpile.grid[1:sandpile.M+1, 1:sandpile.N+1]))
                 totalSteps = time + sandpile.t
         allDensities.append(density)
 
@@ -170,41 +170,41 @@ if __name__ == '__main__':
         #Plots:
         if plotOn:
             obsLegend = ["Size", "Lifetime", "Area", "Radius"]
-            unitsLegend = ["number of sites","time units","number of sites","number of sites"] #units for each observable
-            #Histogram plots and loglog plots for observables vs number avalanches:
+            unitsLegend = ["number of sites", "time units", "number of sites", "number of sites"]  # units for each observable
+            # Histogram plots and loglog plots for observables vs number avalanches:
             i = 0
-            for stat in [size,lifetime, area,radius]:
-                plot_histogram(stat,obsLegend[i],unitsLegend[i])
-                loglog_plot_stats(stat,obsLegend[i],unitsLegend[i])
+            for stat in [size, lifetime, area, radius]:
+                plot_histogram(stat, obsLegend[i], unitsLegend[i])
+                loglog_plot_stats(stat, obsLegend[i], unitsLegend[i])
                 i += 1
 
             # Power law fit of observables vs number of avalanches
             i = 0
-            for stat in [size,lifetime,area]:
-                [stats,freqs] = get_freq_data(stat)
-                power_law_fit_plot(stats,freqs,[obsLegend[i],"Number of avalanches"], [unitsLegend[i],"Number"])
+            for stat in [size, lifetime, area]:
+                [stats, freqs] = get_freq_data(stat)
+                power_law_fit_plot(stats, freqs, [obsLegend[i], "Number of avalanches"], [unitsLegend[i],"Number"])
                 i += 1
 
             # Consider radius separately:  remove zero term from radius in order to get power law fit
-            [radii,freqs] = get_freq_data(radius)
-            radii,freqs  = radii[1:],freqs[1:]
-            power_law_fit_plot(radii,freqs,[obsLegend[3],"Number of avalanches"],[unitsLegend[i],"Number"])
+            [radii, freqs] = get_freq_data(radius)
+            radii, freqs  = radii[1:], freqs[1:]
+            power_law_fit_plot(radii,freqs, [obsLegend[3], "Number of avalanches"], [unitsLegend[i], "Number"])
             # try exponential fit for radius instead:
             params, pcov = curve_fit(exp_func, radii, freqs)
-            plt.plot(radii,freqs,".")
+            plt.plot(radii, freqs, ".")
             plt.plot(np.sort(radii), exp_func(np.sort(radii), *params), '-')
-            plt.legend(["Avalanche data","Exponential fit: ~ exp(-%5.3fx)" %(params[1])])
-            plt.title("Scaling law between %s and %s" %(obsLegend[3],"Number of avalanches") )
-            plt.xlabel("%s (%s)" %(obsLegend[3],unitsLegend[3]))
+            plt.legend(["Avalanche data", "Exponential fit: ~ exp(-%5.3fx)" %(params[1])])
+            plt.title("Scaling law between %s and %s" %(obsLegend[3], "Number of avalanches"))
+            plt.xlabel("%s (%s)" %(obsLegend[3], unitsLegend[3]))
             plt.ylabel("Number of avalanches")
             plt.show()
 
-            #Power law fits of size vs other observables
-            [sizes,freqs] = get_freq_data(size)
+            # Power law fits of size vs other observables
+            [sizes, freqs] = get_freq_data(size)
             i = 1
-            for stat in [lifetime,area,radius]:
-                [stats,statFreqs] = get_freq_data(stat)
-                power_law_fit_plot(size,stat,[obsLegend[0],obsLegend[i]],[unitsLegend[0],unitsLegend[i]]) # correlations between  each variable and avalanche size :
+            for stat in [lifetime, area, radius]:
+                [stats, statFreqs] = get_freq_data(stat)
+                power_law_fit_plot(size, stat, [obsLegend[0], obsLegend[i]], [unitsLegend[0], unitsLegend[i]])  # correlations between  each variable and avalanche size :
                 i += 1
             ##############
 
@@ -219,8 +219,8 @@ if __name__ == '__main__':
         plt.show()
 
         # Grid surface plot of final configuration
-    sandGrid = sandpile.grid[1:sandpile.M+1, 1:sandpile.N+1] #grid without edges
-    plt.imshow(sandGrid, interpolation ='nearest',cmap = 'Blues')
+    sandGrid = sandpile.grid[1:sandpile.M+1, 1:sandpile.N+1]  # grid without edges
+    plt.imshow(sandGrid, interpolation='nearest', cmap='Blues')
     plt.title("Surface Density of Sandpile (Number of Grains: %d)" %(totalGrains))
     plt.colorbar()
     plt.show()
